75e3ba202c7e4b3ebe76ed0a635e0d9ad92e0280
emmanuelabhi643 <emmanuelabhilash3@gmail.com>
2023-12-28 15:27:46 +0530

    initial commit

---
+++ Server/app.js
@@ -0,0 +1,36 @@
+// const bodyparser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const express = require('express');
+const app = express();
+
+
+//middleware
+//app.use(cookieParser());
+//app.use(bodyparser.json());
+app.use(express.json())
+app.use(cors());
+
+//mongoose connection
+mongoose.connect(process.env.Mongodb_uri).then(() => {
+    const port = process.env.port;
+    app.listen(port, () => {
+        console.log("server running on port http://localhost:8080");
+    });
+});
+
+//type of request and url
+// app.use((req, res, next) => {
+//     console.log(`Received ${req.method} request for ${req.url}`);
+//     next();
+// });
+
+app.get('/', (req, res) => {
+    res.json({ message: "succesful connection" });
+});
+
+
+
